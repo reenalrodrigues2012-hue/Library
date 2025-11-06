dab1e66d3bc14036980a8f72cdb2bfb144f1957b
reenalrodrigues2012-hue <reenalrodrigues2012@gmail.com>
2025-11-06 06:56:11 +0000

    testing

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+import Example from "./Example"
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <div>
+      <h1>
+        I am reenal
+        
+      </h1>
+      <Example/>
+      <Example/>
+      <Example/>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Example.jsx
@@ -0,0 +1,17 @@
+import './Example.css';
+
+function Example() {
+ 
+
+  return (
+    <>
+     <div className="example-div">
+      <h1>
+        It is Example
+      </h1>
+     </div>
+    </>
+  )
+}
+
+export default Example
